54cd76e7dfab7d8c6aa113d2a63dce6122e2eb27
Amanda Jauregui <amandajauregui.dev@gmail.com>
2026-04-23 10:44:45 -0300

    feat: refactor Prisma integration by creating PrismaModule and updating service imports

--- product-manager-backend/src/app.module.ts
+++ product-manager-backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
-import { PrismaService } from './prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  imports: [ProductsModule, CategoriesModule, PrismaModule],
 })
 export class AppModule {}
--- product-manager-backend/src/products/products.module.ts
+++ product-manager-backend/src/products/products.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { PrismaService } from '../prisma.service';
-
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [],
-  exports: [],
 })
 export class ProductsModule {}
