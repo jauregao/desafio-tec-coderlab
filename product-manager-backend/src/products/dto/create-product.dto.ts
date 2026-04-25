d6b2d2118c9256a8cd436261aed2b888081c8c1e
Amanda Jauregui <amandajauregui.dev@gmail.com>
2026-04-25 09:51:11 -0300

    refac: refactor CreateProductDto to remove categoryIds and enforce single categoryId

--- product-manager-backend/src/products/dto/create-product.dto.ts
+++ product-manager-backend/src/products/dto/create-product.dto.ts
@@ -1,13 +1,5 @@
 import { Type } from 'class-transformer';
-import {
-  ArrayMinSize,
-  IsArray,
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsString,
-  Min,
-} from 'class-validator';
+import { IsInt, IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateProductDto {
   @IsString({ message: 'Name must be a string' })
@@ -23,9 +15,7 @@ export class CreateProductDto {
   @Min(0, { message: 'Price must be a non-negative number' })
   price: number;
 
-  @IsArray({ message: 'categoryIds must be an array' })
-  @ArrayMinSize(1, { message: 'Product must have at least one category' })
   @Type(() => Number)
-  @IsInt({ each: true, message: 'Each category id must be an integer' })
-  categoryIds: number[];
+  @IsInt({ message: 'categoryId must be an integer' })
+  categoryId: number;
 }
